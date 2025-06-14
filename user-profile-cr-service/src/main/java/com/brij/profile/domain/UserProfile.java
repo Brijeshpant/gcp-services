26d6573836737dd215c1d25c5ac5f7837f7877dc
brijesh <pantbrijesh20@gmail.com>
2025-06-14 17:39:47 +0530

    Cloud run function

--- user-profile-cr-service/src/main/java/com/brij/profile/domain/UserProfile.java
+++
@@ -1,4 +0,0 @@
-package com.brij.profile.domain;
-
-public record UserProfile(String id , String name, String email) {
-}
--- user-profile-cr-service/src/main/java/com/brij/profile/services/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.brij.profile.services;
-
-import com.brij.profile.domain.UserProfile;
-
-public interface UserService {
-    UserProfile getProfile(String userId);
-    UserProfile createUser(UserProfile profile);
-}
